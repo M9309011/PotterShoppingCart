15bc86b7affed486904c539a03d8e8fed556c213
M9309011 <hammer.mailbox@gmil.com>
2015-10-30 12:13:59 +0800

    綠燈 5： 一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375

--- PotterShoppingCartUT_V2/PotterShoppingCartTest.vb
+++ PotterShoppingCartUT_V2/PotterShoppingCartTest.vb
@@ -66,4 +66,22 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Assert.AreEqual(nExpected, nActual)
     End Sub
 
+    <TestMethod()> Public Sub Buy_One_Book1_and_One_Book2_and_One_Book3_and_One_Book4_and_One_Book5_then_Pay_375()
+        'arrang
+        Dim objTarget As PotterShoppingCart = New PotterShoppingCart()
+        Dim nExpected As Integer = 375
+        Dim nActual As Integer = 0
+
+        'act
+        objTarget.add(New Potter(1))
+        objTarget.add(New Potter(2))
+        objTarget.add(New Potter(3))
+        objTarget.add(New Potter(4))
+        objTarget.add(New Potter(5))
+        nActual = objTarget.CheckOut()
+
+        'assert
+        Assert.AreEqual(nExpected, nActual)
+    End Sub
+
 End Class
