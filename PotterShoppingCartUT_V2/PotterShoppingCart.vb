8efb3d3bc92952800a4c217385d59cbbb315e867
M9309011 <hammer.mailbox@gmil.com>
2015-10-30 11:37:44 +0800

    綠燈 3： 如果你買了三本不同的書，則會有10%的折扣

--- PotterShoppingCartUT_V2/PotterShoppingCart.vb
+++ PotterShoppingCartUT_V2/PotterShoppingCart.vb
@@ -11,11 +11,29 @@ Class PotterShoppingCart
 
         '1. 一到五集的哈利波特，每一本都是賣100元
         nReturn = Me.l_objPotters.Count * 100
-        If Me.l_objPotters.Count = 2 _
-            AndAlso Me.l_objPotters(0).Episode <> Me.l_objPotters(1).Episode Then
-            '2. 如果你從這個系列買了兩本不同的書，則會有5%的折扣
-            nReturn = nReturn * 0.95
-        End If
+
+        Select Case Me.l_objPotters.Count
+            Case 2 '如果你買了兩本書
+                If Me.l_objPotters(0).Episode <> Me.l_objPotters(1).Episode Then
+                    '2. 如果你從這個系列買了兩本不同的書，則會有5%的折扣
+                    nReturn = nReturn * 0.95
+                End If
+
+            Case 3 '如果你買了三本書
+                If Me.l_objPotters(0).Episode <> Me.l_objPotters(1).Episode _
+                    AndAlso Me.l_objPotters(0).Episode <> Me.l_objPotters(2).Episode _
+                    AndAlso Me.l_objPotters(1).Episode <> Me.l_objPotters(2).Episode Then
+                    '3. 如果你買了三本不同的書，則會有10%的折扣
+                    nReturn = nReturn * 0.9
+
+                ElseIf Me.l_objPotters(0).Episode <> Me.l_objPotters(1).Episode _
+                    OrElse Me.l_objPotters(0).Episode <> Me.l_objPotters(2).Episode _
+                    OrElse Me.l_objPotters(1).Episode <> Me.l_objPotters(2).Episode Then
+                    '2. 如果你從這個系列買了兩本不同的書，則會有5%的折扣
+                    nReturn = nReturn * 0.95
+                End If
+        End Select
+
         
         Return nReturn
     End Function
