acfb9ffcd1624c06aec8a62f95ebdd92ad36725f
M9309011 <hammer.mailbox@gmil.com>
2015-10-30 12:22:28 +0800

    綠燈 7： 6. 需要留意的是，如果你買了四本書，其中三本不同，第四本則是重複的， …

--- PotterShoppingCartUT_V2/PotterShoppingCartTest.vb
+++ PotterShoppingCartUT_V2/PotterShoppingCartTest.vb
@@ -100,4 +100,20 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Assert.AreEqual(nExpected, nActual)
     End Sub
 
+    <TestMethod()> Public Sub Buy_One_Book1_and_Two_Book2_and_Two_Book3_then_Pay_460()
+        'arrang
+        Dim objTarget As PotterShoppingCart = New PotterShoppingCart()
+        Dim nExpected As Integer = 460
+        Dim nActual As Integer = 0
+
+        'act
+        objTarget.add(New Potter(1))
+        objTarget.add(New Potter(2), 2)
+        objTarget.add(New Potter(3), 2)
+        nActual = objTarget.CheckOut()
+
+        'assert
+        Assert.AreEqual(nExpected, nActual)
+    End Sub
+
 End Class
